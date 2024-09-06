c8651fae751fa457fe6dc828f5703e034bce9d8c
VirGarcia <vir_garcia_minero@hotmail.com>
2024-09-06 13:29:31 +0200

    intermedio

--- Superheroes-iOS/Data/SuperHero.swift
+++ Superheroes-iOS/Data/SuperHero.swift
@@ -17,12 +17,37 @@ struct SuperHero: Codable {
     let id: String
     let name: String
     let image: Image
+    let powerstats: Powerstats
+    let biography: Biography
+    let appearance: Appearance
+    let work: Work
 }
 
 struct Image: Codable {
     let url: String
 }
 
+struct Powerstats: Codable {
+    let intelligence: String?
+}
 
+struct Biography: Codable {
+    let fullName: String?
+    let alignment: String?
+    let publisher: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case alignment, publisher
+        case fullName = "full-name"
+    }
+}
 
+struct Appearance: Codable {
+    let gender: String?
+    let race: String?
+}
+
+struct Work: Codable {
+    let base: String?
+}
 
