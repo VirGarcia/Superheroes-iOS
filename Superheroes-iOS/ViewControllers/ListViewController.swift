3f51718568a534c33747b5881f59098f6e75905f
VirGarcia <vir_garcia_minero@hotmail.com>
2024-09-12 14:13:21 +0200

    error

--- Superheroes-iOS/ViewControllers/ListViewController.swift
+++ Superheroes-iOS/ViewControllers/ListViewController.swift
@@ -7,30 +7,32 @@
 
 import UIKit
 
-class ListViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate{
+class ListViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate, UISearchControllerDelegate{
     
     // MARK: TableView Delegate & DataSource
 
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var notFoundLabel: UILabel!
     
     var superHeroList: [SuperHero] = []
     //var superheroInitialList: [SuperHero] = []
     
-    
-   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        //configureRefreshControl()
         // Setup searchBar
         let search = UISearchController(searchResultsController: nil)
-        //search.delegate = self
+        search.delegate = self
         search.searchBar.delegate = self
         self.navigationItem.searchController = search
+        //configureRefreshControl()
         
         //Setup tableView
         tableView.dataSource = self
+        configureRefreshControl()
         
         // Search data
                searchSuperHeroes("a")
@@ -50,21 +52,7 @@ class ListViewController: UIViewController, UITableViewDataSource, UISearchBarDe
         
         return cell
     }
-    
-    // MARK: SearchBar Delegate
-        func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-            searchSuperHeroes(searchBar.text!)
-        }
-    
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-            searchSuperHeroes("a")
-        }
-        
-        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-            if (searchText.isEmpty) {
-                searchSuperHeroes("a")
-            }
-        }
+
     
     // MARK: Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -80,47 +68,66 @@ class ListViewController: UIViewController, UITableViewDataSource, UISearchBarDe
     }
     
     // MARK: Buisness Logic
-    func searchSuperHeroes(_ query: String) {
-        /*SuperHeroProvider.findSuperHeroesByName(query, withResult: { [unowned self] results in
-         self.superHeroList = results
-         DispatchQueue.main.async {
-         self.tableView.reloadData()
-         }
-         })*/
-        
-        Task {
-            let results = try? await SuperHeroProvider.findSuperHeroesByName(query)
-            if let results = results {
-                self.superHeroList = results
-            } else {
-                self.superHeroList = []
-            }
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-            }
-        }
-        
-    }
-    // MARK: Pull to refresh
+       func searchSuperHeroes(_ query: String) {
+           /*SuperHeroProvider.findSuperHeroesByName(query, withResult: { [unowned self] results in
+               self.superHeroList = results
+               DispatchQueue.main.async {
+                   self.tableView.reloadData()
+               }
+           })*/
+           
+           
+           Task {
+               let results = try? await SuperHeroProvider.findSuperHeroesByName(query)
+               DispatchQueue.main.async {
+                   if let results = results {
+                       self.superHeroList = results
+                       self.notFoundLabel.isHidden = true // Oculta el label si hay resultados
+                     
+                   } else {
+                       self.notFoundLabel.isHidden = false // Muestra el label si no hay resultados
+                       self.notFoundLabel.text = "No hay resultados"
+                       self.superHeroList = []
+                     
+                   }
+                   self.tableView.reloadData()
+                   // Dismiss the refresh control.
+                   //
+                   self.tableView.refreshControl?.endRefreshing()
+               }
+           }
+       }
     
-    func configureRefreshControl () {
-       // Add the refresh control to your UIScrollView object.
-        tableView.refreshControl = UIRefreshControl()
-        tableView.refreshControl?.addTarget(self, action: #selector(handleRefreshControl), for: .valueChanged)
-    }
-        
-    @objc func handleRefreshControl() {
-       // Update your content…
-        SuperHeroProvider.findSuperHeroesByName("a", withResult: { [weak self] results in
-            self?.superHeroList = results
-            DispatchQueue.main.async {
-                self?.tableView.reloadData()
-                // Dismiss the refresh control.
-                self?.tableView.refreshControl?.endRefreshing()
+    
+    // MARK: Pull to refresh
+       func configureRefreshControl() {
+          // Add the refresh control to your UIScrollView object.
+           tableView.refreshControl = UIRefreshControl()
+           tableView.refreshControl?.addTarget(self, action: #selector(handleRefreshControl), for: .valueChanged)
+       }
+           
+       @objc func handleRefreshControl() {
+           // Update your content…
+           searchSuperHeroes("a")
+       }
+       
+       // MARK: SearchBar Delegate
+       func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+           searchSuperHeroes(searchBar.text!)
+       }
+       
+       func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+           self.notFoundLabel.isHidden = true
+           searchSuperHeroes("a")
+       }
+       
+       func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            if (searchText.isEmpty) {
+            searchSuperHeroes("a")
             }
-        })
-    }
-
+        
+        }
+    
 }
 
 
